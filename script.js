22e354ae9c7fca9f84f8d58a6a610009c3cf7284
Your Name <hyderali400@gmail.com>
2023-02-21 13:52:19 +0530

    resolve the conlict in js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // reference to markup elements
 const bodyEle = document.querySelector('body');
 const formsection = document.createElement('section');
@@ -23,42 +22,8 @@ function updateBookList() {
     </ul>
     `;
   }
-=======
-const BookContainer = document.querySelector('.book-container');
-const form = document.getElementById('myForm');
-
-const BookList = [];
-function removeData(index) {
-  BookList.splice(index, 1);
-  addData();
 }
-function addData() {
-  let html = '';
-  for (let i = 0; i < BookList.length; i += 1) {
-    html += `<div class="single-book">
-  <ul>
-      <li>${BookList[i].title}</li>
-      <li>${BookList[i].author}</li>
-  </ul>
-  <button onclick='removeData(" + i + ")'>Remove</button>
-  <hr>
-</div>`;
-  }
-  BookContainer.innerHTML = html;
->>>>>>> 09e941ba3c832e5f60158baa2f16c10dffba75db
-}
-form.addEventListener('submit', (event) => {
-  event.preventDefault(); // prevent the form from submitting
-  const title = form.elements.title.value;
-  const author = form.elements.author.value;
-  const myBook = {
-    title,
-    author,
-  };
-  BookList.push(myBook);
-  form.reset();
 
-<<<<<<< HEAD
 // add initial book list section
 const bookListSection = document.createElement('section');
 bookListSection.classList.add('booklist');
@@ -122,7 +87,3 @@ if (userObject) {
   bookTitle.value = userObject.title;
   bookAuth.value = userObject.author;
 } 
-=======
-  addData();
-});
->>>>>>> 09e941ba3c832e5f60158baa2f16c10dffba75db
